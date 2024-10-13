ea5ca1312ed5debb8f2835d95a1388426947532d
John O'Keeffe <john.ok.dev@gmail.com>
2024-10-13 17:55:41 +0200

    Fix path to SessionRestoreMiddleware in settings.py > MIDDLEWARE.

--- backend/coconut_tree/settings.py
+++ backend/coconut_tree/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'backend.users.middleware.SessionRestoreMiddleware',
+    'users.middleware.SessionRestoreMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
